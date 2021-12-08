dca2e70277ee377acc07b79a076e3d85da10d7e3
Jay <jaytpa01@gmail.com>
2021-12-08 12:50:57 +1100

    Added: New emote

--- pkg/emote/emote.go
+++ pkg/emote/emote.go
@@ -4,4 +4,5 @@ const (
 	EMOTE_JIZ       = "<:jiz:917405912447221820>"
 	EMOTE_IGUSTUS   = "<:iGustus:917389234208399411>"
 	EMOTE_FRIGACHAD = "<:frigachad:917643394203942972>"
+	EMOTE_JIZUS     = "jizus"
 )
